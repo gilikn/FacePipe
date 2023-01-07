789e406019176cd8f74168b45d09f3c48cd2886f
gilikn <giliknn4@gmail.com>
2023-01-07 15:11:55 +0200

    Running script fixes

--- main_face_tracking_pipeline.py
+++ main_face_tracking_pipeline.py
@@ -1,23 +1,19 @@
 import logging
-import os
 import subprocess
 
 from absl import app
 from absl import flags
 
-from facepipe.config.pre_processing_config import CROP_DIRECTORY, AUDIO_DIRECTORY, ORIGINAL_DIRECTORY
+from facepipe.config.pre_processing_config import CROP_DIRECTORY, AUDIO_DIRECTORY
 from facepipe.core.run_face_detection import run_face_detection_pipeline, add_audio_pipeline
 
-flags.DEFINE_string('base_directory_path', '', 'The directory where original and tracked directories are held.')
-flags.DEFINE_string('original_directory_name', ORIGINAL_DIRECTORY,
-                    'Name of directory in which original videos are held.')
+flags.DEFINE_string('base_directory_path', None, 'The path to the directory where original and results directories are held.')
 flags.DEFINE_string('cropped_directory_name', CROP_DIRECTORY,
-                    'Name of directory in which cropped (silent) videos will be saved.')
+                    'Name of directory in which cropped (silent) videos will be saved (under base_directory_path).')
 flags.DEFINE_string('audio_directory_name', AUDIO_DIRECTORY,
-                    'Name of directory in which cropped videos will be saved with audio.')
-flags.DEFINE_string('mean_face_path', '', 'reference mean face (download from: '
-                                     'https://github.com/mpc001/Lipreading_using_Temporal_Convolutional_Networks/blob/master/preprocessing/20words_mean_face.npy)')
-
+                    'Name of directory in which cropped videos will be saved with audio (under base_directory_path).')
+flags.DEFINE_string('mean_face_path', None, "reference mean face (download from: "
+                                     "https://github.com/mpc001/Lipreading_using_Temporal_Convolutional_Networks/blob/master/preprocessing/20words_mean_face.npy)")
 FLAGS = flags.FLAGS
 
 
@@ -25,8 +21,10 @@ def main(argv):
     del argv
     logging.basicConfig()
     logging.root.setLevel(logging.NOTSET)
+    assert FLAGS.mean_face_path is not None, "Please enter the path to the mean_face file!"
+    assert FLAGS.base_directory_path is not None, "Please enter the path to the videos!"
     directory_path = FLAGS.base_directory_path
-    videos_dir_path = os.path.join(directory_path, ORIGINAL_DIRECTORY)
+    videos_dir_path = directory_path
     subprocess.call(f'mkdir {FLAGS.cropped_directory_name}', cwd=directory_path, shell=True)
     subprocess.call(f'mkdir {FLAGS.audio_directory_name}', cwd=directory_path, shell=True)
     run_face_detection_pipeline(directory_path, FLAGS.mean_face_path, videos_dir_path)
